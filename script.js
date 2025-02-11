93ecb02d97bde6ef832fbb1f9fc5404100e574c5
baisali609 <baisali609@gmail.com>
2025-02-11 10:45:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,42 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model){
+	this.make = make;
+	this.model = model;
+}
 
-function SportsCar(make, model, topSpeed) {}
+function SportsCar(make, model, topSpeed) {
+	Car.call(this,make,model);
+	this.topSpeed = topSpeed;
+}
+
+
+
+// class Car {
+// 	constructor (make, model){
+// 		this.make = make;
+// 		this.model = model;
+// 	}
+	
+// }
+
+Car.prototype.getMakeModel = function(){
+		return `${this.make} ${this.model}`;
+	}
+
+
+// class SportsCar extends Car{
+// 	constructor(make, model, topSpeed) {
+// 		super(make, model);
+// 		this.topSpeed = topSpeed;
+// 	}
+	
+// }
+
+SportsCar.prototype.getTopSpeed = function(){
+		return this.topSpeed;
+	}
+
+SportsCar.prototype = Object.create(Car.prototype);
 
 // Do not change the code below
 window.Car = Car;
